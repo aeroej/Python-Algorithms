b05d2f67a6c412cb0c00e9d08e8644e75ebef0c8
aeroej <asd0755@naver.com>
2021-10-29 16:36:12 +0900

    실버 퇴사 수정 재풀이

--- baekjoon/silver/퇴사.py
+++ baekjoon/silver/퇴사.py
@@ -1,22 +1,49 @@
+# import sys
+
+# def dfs(depth, money):
+#   global max_money
+#   if depth == n:  
+#     max_money = max(max_money, money)
+#     return
+#   elif depth > n: 
+#     return
+  
+#   t, p = table[depth]
+#   dfs(depth+t, money + p) 
+#   dfs(depth+1, money)
+    
+
+# if __name__ == "__main__":
+#   n = int(input())
+#   table = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+#   max_money = 0
+
+#   dfs(0, 0)
+#   print(max_money)
+
+
+
 import sys
 
-def dfs(depth, money):
-  global max_money
-  if depth == n:  
-    max_money = max(max_money, money)
+def dfs(depth, price):
+  global max_price
+  if depth == n:
+    max_price = max(max_price, price)
     return
-  elif depth > n: 
+
+  if depth > n:
     return
-  
-  t, p = table[depth]
-  dfs(depth+t, money + p) 
-  dfs(depth+1, money)
-    
+
+  t, p = seq[depth]
+  dfs(depth + t, price + p)
+  dfs(depth + 1, price)
+
 
 if __name__ == "__main__":
   n = int(input())
-  table = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-  max_money = 0
+  seq = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
+  max_price = 0
   dfs(0, 0)
-  print(max_money)
+  print(max_price)
+
