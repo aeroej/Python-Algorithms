f1de01fc9a021fb31b6eee141762bf7f94830757
aeroej <asd0755@naver.com>
2021-11-03 01:22:01 +0900

    Modify 암호만들기.py Add DFS solution

--- baekjoon/gold/암호만들기.py
+++ baekjoon/gold/암호만들기.py
@@ -1,3 +1,4 @@
+# combinations 
 from itertools import combinations
 
 L, C = map(int,input().split())
@@ -12,3 +13,33 @@ for word in words:
     
     if cnt_vow >=1 and L - cnt_vow >=2:
         print(''.join(word))
+
+
+
+# dfs
+import sys
+
+def dfs(depth, stack, vowel):
+    if len(stack) >= l:
+        if (vowel >= 1) and (l - vowel >= 2):
+            print(''.join(stack))
+        return 
+
+    if depth >= c:
+        return
+
+    stack.append(seq[depth])
+    if seq[depth] in 'aeiou':
+        dfs(depth+1, stack, vowel + 1)
+    else:
+        dfs(depth+1, stack, vowel)
+
+    stack.pop()
+    dfs(depth+1, stack, vowel)
+
+
+if __name__ == "__main__":
+    l, c = map(int, sys.stdin.readline().split())
+    seq = sorted(sys.stdin.readline().split())
+
+    dfs(0, [], 0)
