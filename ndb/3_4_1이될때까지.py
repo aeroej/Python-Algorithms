dc24a8220cd20efd9ded2d7bde76fbb0e1dbbe67
aeroej <asd0755@naver.com>
2021-12-20 00:15:38 +0900

    Create 3_4_1이될때까지.py

---
+++ ndb/3_4_1이될때까지.py
@@ -0,0 +1,19 @@
+import sys
+si = sys.stdin.readline
+LIMIT = 100_000
+
+n, k = map(int, si().split())
+
+for cnt in range(LIMIT):
+  if n == 1:
+    print(cnt)
+    break
+
+  if n % k == 0:
+    n = int(n/k)
+
+  else:
+    n -= 1
+    
+
+
