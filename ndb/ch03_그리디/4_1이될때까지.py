22544dfe02d1ea7a2982bf70eeefbc8f910dcd5a
aeroej <asd0755@naver.com>
2021-12-20 01:55:48 +0900

    Modify ch03 4_1이될때까지.py

--- ndb/ch03_그리디/4_1이될때까지.py
+++ ndb/ch03_그리디/4_1이될때까지.py
@@ -1,3 +1,4 @@
+# 단순 
 import sys
 si = sys.stdin.readline
 LIMIT = 100_000
@@ -14,3 +15,21 @@ for cnt in range(LIMIT):
 
   else:
     n -= 1
+
+
+# 그리디 
+import sys
+n, k = map(int, si().split())
+cnt = 0
+
+while n > 1:
+    if n % k == 0:
+        n = int(n / k)
+        cnt += 1
+    else:
+        cnt += (n % k) 
+        n -= (n % k)
+
+print(cnt if n else cnt - 1)
+
+
